815ec0204c125aceb70713f3bf1feba48052a3b4
yoomster <nperenboom@hotmail.com>
2024-02-21 15:56:06 +0100

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import ToDo from "./pages/ToDo";
 import Contact from "./pages/Contact";
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 import './App.css'
 
-
 function App () {
-
   return (
     <>  
-    <BrowserRouter>
+    <Grid container spacing={2}>
+      <BrowserRouter>
+      <Grid md={4} >
 
-      <div className="nav-bar">
-        <ul>
+          <ul className="nav-bar">
           <ol>
             <Link to="/home">Home</Link>
           </ol>
@@ -29,17 +29,23 @@ function App () {
           <ol>
             <Link to="/contact">Contact us</Link>
           </ol>
-        </ul>
+          </ul>
+        </Grid>
 
-    <Routes>
-      <Route  path="/" element={<Home />} />
-        <Route path="About" element={<About />} />
-        <Route path="ToDo" element={<ToDo />} />
-        <Route path="Contact" element={<Contact />} />
-    </Routes>
-    </div>
 
-  </BrowserRouter>
+        <Grid md={8} >
+          <Routes>
+
+            <Route  path="/" element={<Home />} />
+            <Route path="About" element={<About />} />
+            <Route path="ToDo" element={<ToDo />} />
+            <Route path="Contact" element={<Contact />} />
+
+          </Routes>
+        </Grid>
+
+      </BrowserRouter>
+    </Grid>
   </>
   )
 }
